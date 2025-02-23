43d37006eea1f10f68723109e0a3ed173174f922
abonvalle <abonvalle@proton.me>
2025-02-23 22:42:31 +0100

    Add internationalization support for carousel and update styles

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Carousel as LibCarousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from "./carousel.module.css";
 import { CarouselImage } from "./image.interface";
 
 const Carousel = ({ images }: { images: CarouselImage[] }) => {
+  const { t } = useTranslation();
   const [isHovered, setIsHovered] = useState<boolean>(false);
   const [isExpanded, setIsExpanded] = useState<CarouselImage | null>(null);
   const expand = (image: CarouselImage) => {
@@ -38,7 +40,7 @@ const Carousel = ({ images }: { images: CarouselImage[] }) => {
               onClick={() => expand(image)}
             >
               <img src={image.url} alt={image.alt} />
-              <p className="legend">Cliquer pour agrandir</p>
+              <p className="legend">{t("click_to_zoom")}</p>
             </div>
           ))}
         </LibCarousel>
